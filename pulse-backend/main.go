10bbad784ca4f5a66666ccdae3bdbc0d785fe233
sandeshnilas <sandeshnilaskhatwada@gmail.com>
2026-05-26 09:55:07 +0545

    creates basic initiate the data

--- pulse-backend/main.go
+++ pulse-backend/main.go
@@ -6,10 +6,17 @@ import (
 
 	"github.com/SandeshNilasKhatiwada/slack-clone/internal/auth"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+
+	//adding cors
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:4200",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+	}))
 	//connect to the database
 	data, err := database.Connect()
 	// Create a GET route for our health check
@@ -30,13 +37,12 @@ func main() {
 			"message": "Database connection successful",
 		})
 	})
-	
+
 	app.Post("/api/user/register", auth.Register)
 	app.Post("/api/user/login", auth.Login)
-	
+
 	user := app.Group("/api/user", middleware.RequireAuth)
 	user.Get("/me", auth.GetMe)
-
 
 	// Start the server on port 8080
 	app.Listen(":8080")
---
+++ pulse-frontend/src/app/core/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+export interface AuthResponse {
+  message: string;
+  token?: string;
+  user?: {
+    id: number;
+    username: string;
+    email: string;
+  }
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:8080/api/auth';
+
+  register(userData: any) {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/register`, userData);
+  }
+
+  login(credentials: any) {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/login`, credentials);
+  }
+
+  // helper to save token to localStorage
+  saveToken(token: string) {
+    localStorage.setItem('authToken', token);
+  }
+}
